7b68d2cd405d2e44c04555842df3b64295972ea4
Denis Shtefan <den.shtefan@gmail.com>
2019-07-18 21:33:50 +0300

    Add MainPage

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,3 +1,4 @@
+import com.wrike.pages.MainPage;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,7 +13,7 @@ public class MainTest {
 
     @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "~/IdeaProjects/ChromeDriver/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/home/denis/IdeaProjects/ChromeDriver/chromedriver");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
@@ -20,7 +21,9 @@ public class MainTest {
 
     @Test
     public void myTest() {
-
+        driver.get("https://www.wrike.com/");
+        MainPage mainPage = new MainPage(driver);
+        mainPage.createAccount("weqeqweqwe@wqdw.ru");
     }
 
     @After
